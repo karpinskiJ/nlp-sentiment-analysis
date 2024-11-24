0cb8200ec97eae61b377ea11b43b6809c0bc6b37
karpinskiJ <01150169@pw.edu.pl>
2024-11-24 21:55:09 +0100

    init commit

---
+++ nlp-sentiment-analysis/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='nlp-sentiment-analysis',
+    version='0.0.0',
+    description='NLP Sentiment Analysis using LSTM for Twitter Sentiment Analysis Dataset',
+    author='Jakub Karpiński, Weronika Skiba',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
+    
